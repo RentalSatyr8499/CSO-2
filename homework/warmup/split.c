8dc18d6d1572fd761d80930796268b3b1b53649b
Yeeva Chunnanond <xaq7pj@virginia.edu>
2026-01-29 16:50:18 -0500

    debugging \t bug in split.c

--- homework/warmup/split.c
+++ homework/warmup/split.c
@@ -1,12 +1,19 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include <stdio.h>
+
 char **string_split(const char *input, const char *sep, int *num_words){
     *num_words = 0;
     char **words = (char **)malloc(sizeof(char *)*1);
 
+    printf("separator: %s\n", sep);
+
     int baseIndex = 0, separatorLen = 0, wordLen = 0;
-    while (input[baseIndex] != '\0'){
+    while (1){
+        if (input[baseIndex] == '\0'){
+            break;
+        }
         wordLen = strcspn(&input[baseIndex], sep);
         separatorLen = strspn(&input[baseIndex+wordLen], sep);
 
@@ -22,6 +29,8 @@ char **string_split(const char *input, const char *sep, int *num_words){
         words[*num_words-1][wordLen] = '\0';
         baseIndex += separatorLen + wordLen;
     }
+
+    printf("found null byte at %d, returning words", baseIndex);
 
     return words;
 }
