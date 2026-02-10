2ad4be47410a062d23002fe04c5c3f2345fe3e6b
Yeeva Chunnanond <xaq7pj@virginia.edu>
2026-02-10 16:13:25 -0500

    mostly done with fork hw

--- homework/fork/fork_run.c
+++ homework/fork/fork_run.c
@@ -2,14 +2,17 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void writeoutput(const char *command, const char *out_path, const char *err_path){
     pid_t p = fork();
     if (p == 0){
-        int out = open(out_path, O_WRONLY | O_CREAT);
+        int out = open(out_path, O_WRONLY | O_CREAT | O_APPEND, 0);
         dup2(out, STDOUT_FILENO);
         
-        int err = open(err_path, O_WRONLY | O_CREAT);
+        int err = open(err_path, O_WRONLY | O_CREAT | O_APPEND);
         dup2(err, STDERR_FILENO);
         
         execl( "/bin/sh", "sh", "-c", command, (char *)NULL );
@@ -21,37 +24,60 @@ void writeoutput(const char *command, const char *out_path, const char *err_path
     return;
 }
 
+void writegetoutput(){
+    return;
+}
 void parallelwriteoutput(int count, const char **argv_base, const char *out_path){
     // step 1: build argv
-    char **argv = malloc((len(argv_base)+1)*sizeof(char**)); // one extra spot for child index
+    char **argv = NULL;
     int i = 0;
     while (argv_base[i] != NULL){
-        argv[i] = argv_base[i];
+        argv = realloc(argv, (i+1)*sizeof(char*));
+        argv[i] = strdup(argv_base[i]);
         i++;
     }
-    argv[i+1] = NULL; // skip over the second to last spot for the child index
-
+    
+    argv = realloc(argv, (i+2) * sizeof(char*)); // space for child index and NULL terminator 
+    argv[i] = NULL; 
+    argv[i+1] = NULL; 
     pid_t childPIDs[count];
     pid_t p = 1;
-    for (int j = 0; j < count; j++){
+
+    // step 2: run commands 
+    int original_stdout = dup(STDOUT_FILENO); // redirect to out.txt
+    int out = open(out_path, O_WRONLY | O_CREAT | O_APPEND, 0);
+    dup2(out, STDOUT_FILENO); 
+    
+    for (int j = 0; j < count; j++){ // run count children
         p = fork();
-        int out = open(out_path, O_WRONLY | O_CREAT);
-        dup2(out, STDOUT_FILENO); // redirect output to out.txt
-        if (p == 0){ // child branch
-            argv[i] = itoa(j); // fill in child index
-            execv(
-                argv_base[0], 
-                argv
-            );
+        if (p == 0){ // child branch   
+            char *idx_str = malloc(16); // fill in child index
+            snprintf(idx_str, 16, "%d", j);
+            argv[i] = idx_str;
+            argv[i+1] = NULL;
+
+            execv(argv_base[0], argv);
+            
+            free(idx_str);
+            close(out);
+            perror("execv");
+            _exit(1);            
         } else {
             childPIDs[j] = p; // save child PID so we can wait on it later
         }
     }
 
-    int status;
+    // step 3. clean up
     for (int k = 0; k < count; k++){
-        waitpid(childPIDs[k], &status, 0); // wait on all the childPIDs we stored earlier
+        waitpid(childPIDs[k], NULL, 0); // wait on all the childPIDs we stored earlier
     }
 
+    close(out); // restore stdout
+    dup2(original_stdout, STDOUT_FILENO);
+
+    for (int k = 0; k <= i; k++){ // free dynamic memory
+        free(argv[k]);
+    }
+    free(argv);
     return;
 }
--- homework/fork/main.c
+++ homework/fork/main.c
@@ -2,7 +2,20 @@
 #include <stdio.h>
 
 int main(int argc, const char *argv[]) {
+    
+    printf("Test 1\n");
     printf("Hi!\n");
     writeoutput("echo 1 2 3; sleep 2; echo 5 5", "out.txt", "err.txt");
     printf("Bye!\n");
+
+    printf("Test 2\n");
+    const char *argv_base[] = {
+        "/bin/echo", "running", NULL
+    };
+    parallelwriteoutput(2, argv_base, "out1.txt");
+
+    printf("Test 3\n");
+    const char *argv_base1[] = { "./args.py", "first", "second", "third", NULL };
+    parallelwriteoutput(3, argv_base1, "out2.txt");
+
 }
