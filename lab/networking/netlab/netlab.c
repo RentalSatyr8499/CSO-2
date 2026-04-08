9eddfec2a1b5669449499a2ae6866363c4eb328d
Yeeva Chunnanond <xaq7pj@virginia.edu>
2026-04-08 13:18:56 -0400

    done omg

--- lab/networking/netlab/netlab.c
+++ lab/networking/netlab/netlab.c
@@ -5,6 +5,7 @@ int next_sequence_num = 1;
 int total_sequence_count = 0;
 int current_timer_id = 0;
 char last_ack[5] = {0, 0, 1, 0, 0};
+char get[5];
 
 void fill_checksum(size_t len, void *_data){
     char *data = _data;
@@ -13,6 +14,15 @@ void fill_checksum(size_t len, void *_data){
         checksum ^= data[i];
     }
     data[0] = checksum;
+}
+int verify_packet(int len, void* _data){
+    char *data = _data;
+    char checksum = 0;
+    for (int i = 1; i < len; i++){
+        checksum ^= data[i];
+    }
+
+    return (checksum == data[0]) ? 0 : -1;
 }
 void new_ack(void* _data){
     char ack[5] = {0, 'A', 'C', 'K', ((char *)_data)[1]};
@@ -24,7 +34,7 @@ void new_ack(void* _data){
 }
 void send_ack(){
     send(5, last_ack);
-    if (next_sequence_num <= total_sequence_count) {
+    if (total_sequence_count == 0 ||  next_sequence_num <= total_sequence_count) {
         current_timer_id = setTimeout(send_ack, 1000, NULL);
     }
 }
@@ -36,7 +46,13 @@ void request_next(void* _data){
 }
 void recvd(size_t len, void* _data) {
     char *data = _data;
-
+    if (verify_packet(len, data) == -1){
+        if (total_sequence_count == 0)
+            send(5, get);
+        else
+            send_ack();
+        return;
+    }
     if (data[1] == next_sequence_num){
         clearTimeout(current_timer_id);
         next_sequence_num += 1;
@@ -56,6 +72,9 @@ void send_get(void* arg){
     fill_checksum(5, data);
 
     send(5, data);
+    for (int i = 0; i < 5; i ++){
+        get[i] = data[i];
+    }
 }
 int main(int argc, char *argv[]) {
     if (argc != 2) {
