d757975ac49c3fb1764e70e8869ec9ffb0323e19
Yeeva Chunnanond <xaq7pj@virginia.edu>
2026-04-14 21:03:15 -0400

    working on pool

--- homework/pool/threadpool.c
+++ homework/pool/threadpool.c
@@ -7,16 +7,12 @@ typedef struct Task {
     task_fn fn;
     void *arg;
     void *result;
-
-    int id;
-    struct Task *prev;
-    struct Task *next;
 } Task;
 typedef struct TaskQueue {
-    Task *head;
-    Task *tail;
-    
-    int id_counter;
+    Task *queue[200];
+    int tail_index;
+    int head_index;
+
     pthread_mutex_t lock;
     pthread_cond_t task_ready;
 } TaskQueue;
@@ -27,15 +23,24 @@ pthread_t *thread_pool;
 
 void *worker_thread(void* _info){
     pthread_mutex_lock(&task_queue.lock);
-    // finish
+    while (task_queue.head_index == task_queue.tail_index){
+        pthread_cond_wait(&task_queue.task_ready, &task_queue.lock);
+    }
+    int id = task_queue.head_index;
+    task_queue.head_index += 1;
+    pthread_mutex_unlock(&task_queue.lock);
+
+    task_fn fn = task_queue.queue[id]->fn;
+    void *arg = task_queue.queue[id]->arg;
+    task_queue.queue[id]->result = fn(arg); // i suppose because this is not under the task queue lock it's unsafe, but the logic of the program should guarantee only one thread modifies a given Task's result at a time. And adding the lock will impact performance, too, right? - updating the result needs to be scheduled with all other task queue stuff.
+
 }
 void pool_setup(int threads){
     pthread_mutex_lock(&task_queue.lock);
     pthread_mutex_init(&task_queue.lock, NULL);
     pthread_cond_init(&task_queue.task_ready, NULL);
-    task_queue.id_counter = 0;
-    task_queue.head = NULL;
-    task_queue.tail = NULL;
+    task_queue.head_index = 0;
+    task_queue.tail_index = 0;
     pthread_mutex_unlock(&task_queue.lock);
 
     thread_pool = malloc(sizeof(pthread_t) * threads);
@@ -46,18 +51,14 @@ void pool_setup(int threads){
 
 int pool_submit_task(task_fn task, void *argument){
     pthread_mutex_lock(&task_queue.lock);
-    int id = task_queue.id_counter ++;
-    Task *old_tail = task_queue.tail->next;
     Task *new_task = malloc(sizeof(Task));
     *new_task = (Task){
         task,           // taskfn
         argument,       // arg
         NULL,           // result
-        id,             // id
-        old_tail,       // prev
-        NULL            // next
     };
-    if (task_queue.head == NULL) task_queue.head = new_task;
+    task_queue.queue[task_queue.tail_index] = new_task;
+    int id = task_queue.tail_index ++;
     pthread_cond_signal(&task_queue.task_ready);
     pthread_mutex_unlock(&task_queue.lock);
 
@@ -65,7 +66,7 @@ int pool_submit_task(task_fn task, void *argument){
 }
 
 void *pool_get_task_result(int task_id){
-
+    return (void *)(task_queue.queue[task_id]);
 }
 
 void pool_stop(void){
